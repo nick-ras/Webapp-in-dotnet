998b5ef722c1aa17a4c58e16290f72e4115a3861
Nick R <Nick-ras@mail.dk>
2022-10-10 15:07:58 +0200

    first version of webapp

---
+++ WebAppStartup/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebAppStartup.Models;
+
+namespace WebAppStartup.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<WebAppStartup.Models.Joke> Joke { get; set; }
+    }
+}
---
+++ WebAppStartup/Models/Joke.cs
@@ -0,0 +1,13 @@
+﻿namespace WebAppStartup.Models
+{
+    public class Joke
+    {
+        public int Id { get; set; }
+        public string Question { get; set; }
+        public string Answer { get; set; }
+        public Joke()
+        {
+
+        }
+    }
+}
